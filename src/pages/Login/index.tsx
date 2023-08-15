2a38cd6e04ae40da8c9979f936cc36fe02acd7fe
Lucas dos Santos <lucas.dos.santos0330@gmail.com>
2023-08-15 16:23:30 -0300

    teste fetch

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,20 @@
+import { Box } from "@mui/material";
 import React from "react";
 
-const Login = () => {
+const Login = async () => {
+  try {
+    const result = await fetch("https://randomuser.me/api?results=10").then(
+      (response) => response.json()
+    );
+
+    console.log(result.results);
+  } catch (error) {
+    console.log("ERRO");
+  }
   return (
     <React.Fragment>
       <h1>Login</h1>
+      <Box>oi</Box>
     </React.Fragment>
   );
 };
---
+++ src/store/index.tsx
@@ -0,0 +1,62 @@
+import { hookstate } from "@hookstate/core";
+
+interface StateGlobal {
+  gender: string;
+  name: {
+    title: string;
+    first: string;
+    last: string;
+  };
+  location: {
+    street: {
+      number: number;
+      name: string;
+    };
+    city: string;
+    state: string;
+    country: string;
+    postcode: string;
+    coordinates: {
+      latitude: string;
+      longitude: string;
+    };
+    timezone: {
+      offset: string;
+      description: string;
+    };
+  };
+  email: string;
+  login: {
+    uuid: string;
+    username: string;
+    password: string;
+    salt: string;
+    md5: string;
+    sha1: string;
+    sha256: string;
+  };
+  dob: {
+    date: string;
+    age: number;
+  };
+  registered: {
+    date: string;
+    age: number;
+  };
+  phone: string;
+  cell: string;
+  id: {
+    name: string;
+    value: string;
+  };
+  picture: {
+    large: string;
+    medium: string;
+    thumbnail: string;
+  };
+  nat: string;
+}
+
+const StoreStateGlobal = hookstate<StateGlobal[] | undefined>(undefined);
+
+export default StoreStateGlobal;
