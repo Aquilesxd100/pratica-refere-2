7c5a35ba4cc8764ed87d3b53c755cec957333389
fernando <fernandoalanfillmann@gmail.com>
2023-08-16 16:39:09 -0300

    refactor: implementacao de sistema de Grid

--- src/components/CriminalUpdateModal.tsx
+++ src/components/CriminalUpdateModal.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { ModalBackgroundStyled, ModalBackgroundBlurStyled, ModalBodyStyled } from './CriminalUpdateModalStyled';
-import { Box, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField } from '@mui/material';
+import { Autocomplete, Box, Button, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
 import { Controller, useForm } from "react-hook-form";
+import { dangerLevels, offenses, weapons } from '../utils/personProcessor';
+import SendIcon from '@mui/icons-material/Send';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 /* interface CriminalUpdateModalProps {
   property: string;
@@ -45,45 +48,78 @@ const CriminalUpdateModal: React.FC = () => {
 
   return (
     <ModalBackgroundStyled style={modalProps}>
-        <ModalBackgroundBlurStyled>
-            <ModalBodyStyled>
-                <form>
-                    <Controller control={control}  name="name" render={({ field }) => (
-                        <TextField {...field} label="Name" variant="outlined" />
-                    )} />
+        <ModalBackgroundBlurStyled component="form">
+                <ModalBodyStyled container spacing={2}>
+                    <Grid item xs={6}>
+                        <Controller control={control}  name="name" render={({ field }) => (
+                                <TextField fullWidth {...field} label="Name" variant="outlined" />
+                        )} />
+                    </Grid>
 
-                    <Controller control={control} name="age" render={({ field }) => (
-                        <TextField {...field} type="number" label="Age" variant="outlined" />
-                    )} />
+                    <Grid item xs={6}>
+                        <Controller control={control} name="age" render={({ field }) => (
+                                <TextField fullWidth {...field} type="number" label="Age" variant="outlined" />
+                        )} />
+                    </Grid>
 
-                    <Controller control={control} name="gender" render={({ field }) => (
-                        <Box>
-                            <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                            <RadioGroup {...field} row sx={{display: "flex", justifyContent: "center"}} aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
-                                <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            </RadioGroup>
-                        </Box>
-                    )} />     
+                    <Grid item xs={6}>
+                        <Controller control={control} name="country" render={({ field }) => (
+                                <TextField fullWidth {...field} label="Country" variant="outlined" />
+                            )} /> 
+                    </Grid>
 
-                    <Controller control={control} name="country" render={({ field }) => (
-                        <TextField {...field} label="Country" variant="outlined" />
-                    )} />
+                    <Grid item xs={6}>
+                        <Controller control={control} name="dangerLevel" render={({ field }) => (
+                            <Autocomplete
+                            disablePortal
+                            options={dangerLevels}
+                            renderInput={(params) => <TextField {...field} {...params} label="Danger Level" />}
+                          />
+                            )} />    
+                    </Grid>
 
-                    <Controller control={control} name="dangerLevel" render={({ field }) => (
-                        <Box>
-                            <InputLabel id="demo-simple-select-label">Danger Level</InputLabel>
-                            <Select {...field} labelId="demo-simple-select-label" id="demo-simple-select">
-                                <MenuItem value={"Low"}>Low</MenuItem>
-                                <MenuItem value={"Medium"}>Medium</MenuItem>
-                                <MenuItem value={"High"}>High</MenuItem>
-                            </Select>
-                        </Box>
-                    )} />               
+                    <Grid item xs={6}>
+                        <Controller control={control} name="weapon" render={({ field }) => (
+                            <Autocomplete
+                            disablePortal
+                            options={weapons}
+                            renderInput={(params) => <TextField {...field} {...params} label="Weapon" />}
+                          />
+                            )} />    
+                    </Grid>
 
-  
-                </form>
-            </ModalBodyStyled>               
+                    <Grid item xs={6}>
+                        <Controller control={control} name="offense" render={({ field }) => (
+                            <Autocomplete
+                            disablePortal
+                            options={offenses}
+                            renderInput={(params) => <TextField {...field} {...params} label="Offense" />}
+                          />
+                            )} />    
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Controller control={control} name="gender" render={({ field }) => (
+                                <Box display="flex" flexDirection="column" alignItems="center">
+                                    <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+                                    <RadioGroup {...field} row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
+                                        <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                        <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                    </RadioGroup>
+                                </Box>
+                            )} />   
+                    </Grid>    
+                    <Grid item xs={6}>
+                        <Button variant="contained" endIcon={<SendIcon />}>
+                            Update
+                        </Button>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Button variant="contained" endIcon={<CancelIcon />}>
+                            Cancel
+                        </Button>
+                    </Grid>                
+                </ModalBodyStyled>            
         </ModalBackgroundBlurStyled>
     </ModalBackgroundStyled>
   );
