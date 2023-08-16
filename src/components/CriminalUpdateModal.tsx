97b906960cf6599709a2005bd0aa87c5cf2e4971
fernando <fernandoalanfillmann@gmail.com>
2023-08-16 17:55:45 -0300

    feat: implementa sistema do useForm

--- src/components/CriminalUpdateModal.tsx
+++ src/components/CriminalUpdateModal.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ModalBackgroundStyled, ModalBackgroundBlurStyled, ModalBodyStyled } from './CriminalUpdateModalStyled';
 import { Autocomplete, Box, Button, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField } from '@mui/material';
-import { Controller, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { dangerLevels, offenses, weapons } from '../utils/personProcessor';
 import SendIcon from '@mui/icons-material/Send';
 import CancelIcon from '@mui/icons-material/Cancel';
+import { CriminalsDataType, useGlobalStateCriminals } from '../store/StateGlobal';
 
-/* interface CriminalUpdateModalProps {
-  property: string;
-} */
 const styleModalProps = {
   on: {
     opacity: "1",
@@ -30,8 +28,14 @@ interface FormInfosType {
   dangerLevel: "High" | "Medium" | "Low";
 }
 
-const CriminalUpdateModal: React.FC = () => {
-  const { reset, control } = useForm<FormInfosType>({
+interface CriminalUpdateProps extends FormInfosType {
+  uuid: string;
+}
+
+const CriminalUpdateModal = (props: CriminalUpdateProps) => {
+  const criminals = useGlobalStateCriminals();
+
+  const { reset, control, handleSubmit } = useForm<FormInfosType>({
     defaultValues: {
       name: "",
       age: 0,
@@ -43,11 +47,39 @@ const CriminalUpdateModal: React.FC = () => {
     },
   });
 
-  const [modalProps, setModalProps] = useState(styleModalProps.off);
+  useEffect(() => {
+    if (props) {
+      setModalProps(styleModalProps.on);
+      reset(props);
+    }    
+  }, [props])
+
+  const [modalProps, setModalProps] = useState(styleModalProps.on);
+
+  const updateHandler : SubmitHandler<FormInfosType> = (formValues: FormInfosType) => {
+    let currentCriminals = criminals.get();
+    currentCriminals = currentCriminals.map((criminal: CriminalsDataType) => {
+      if (props.uuid = criminal.uuid) {
+        return {
+          uuid: criminal.uuid,
+          name: formValues.name,
+          age: formValues.age,
+          gender: formValues.gender,
+          country: formValues.country,
+          weapon: formValues.weapon,
+          offense: formValues.offense,
+          dangerLevel: formValues.dangerLevel,
+          img: criminal.img
+        }
+      }
+      return criminal;
+    })
+    criminals.set(currentCriminals as any);
+  }
 
   return (
     <ModalBackgroundStyled style={modalProps as any}>
-        <ModalBackgroundBlurStyled component="form">
+        <ModalBackgroundBlurStyled component="form" onSubmit={handleSubmit(updateHandler)}>
                 <ModalBodyStyled container spacing={2}>
                     <Grid item xs={6}>
                         <Controller control={control}  name="name" render={({ field }) => (
@@ -70,9 +102,10 @@ const CriminalUpdateModal: React.FC = () => {
                     <Grid item xs={6}>
                         <Controller control={control} name="dangerLevel" render={({ field }) => (
                             <Autocomplete
+                            onChange={((e, value: string | null) => {field.onChange(value)})}
                             disablePortal
                             options={dangerLevels}
-                            renderInput={(params) => <TextField {...field} {...params} label="Danger Level" />}
+                            renderInput={(params) => <TextField {...params} {...params} label="Danger Level" />}
                           />
                             )} />    
                     </Grid>
@@ -80,9 +113,10 @@ const CriminalUpdateModal: React.FC = () => {
                     <Grid item xs={6}>
                         <Controller control={control} name="weapon" render={({ field }) => (
                             <Autocomplete
+                            onChange={((e, value: string | null) => {field.onChange(value)})}
                             disablePortal
                             options={weapons}
-                            renderInput={(params) => <TextField {...field} {...params} label="Weapon" />}
+                            renderInput={(params) => <TextField {...params} {...params} label="Weapon" />}
                           />
                             )} />    
                     </Grid>
@@ -90,9 +124,10 @@ const CriminalUpdateModal: React.FC = () => {
                     <Grid item xs={6}>
                         <Controller control={control} name="offense" render={({ field }) => (
                             <Autocomplete
+                            onChange={((e, value: string | null) => {field.onChange(value)})}
                             disablePortal
                             options={offenses}
-                            renderInput={(params) => <TextField {...field} {...params} label="Offense" />}
+                            renderInput={(params) => <TextField {...params} label="Offense" />}
                           />
                             )} />    
                     </Grid>
@@ -108,16 +143,18 @@ const CriminalUpdateModal: React.FC = () => {
                                 </Box>
                             )} />   
                     </Grid>    
-                    <Grid item xs={6}>
-                        <Button variant="contained" endIcon={<SendIcon />}>
-                            Update
-                        </Button>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Button variant="contained" endIcon={<CancelIcon />}>
+                    <Grid item xs={12} spacing={4} container justifyContent="center">
+                        <Grid item xs={5}>
+                          <Button type="submit" onClick={(() => { setModalProps(styleModalProps.off) })} fullWidth variant="contained" color="success" endIcon={<SendIcon />}>
+                              Update
+                          </Button>
+                        </Grid>
+                        <Grid item xs={5}>
+                          <Button onClick={(() => { setModalProps(styleModalProps.off); reset(); })} fullWidth variant="contained" color="error" endIcon={<CancelIcon />}>
                             Cancel
-                        </Button>
-                    </Grid>                
+                          </Button>
+                        </Grid>  
+                    </Grid>              
                 </ModalBodyStyled>            
         </ModalBackgroundBlurStyled>
     </ModalBackgroundStyled>
