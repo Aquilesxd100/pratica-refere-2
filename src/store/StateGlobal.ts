2a603312b46073ac6e856389f1fb98c2f2a9f31d
Lucas dos Santos <lucas.dos.santos0330@gmail.com>
2023-08-16 16:37:52 -0300

    refactor: config array police

--- src/store/StateGlobal.ts
+++ src/store/StateGlobal.ts
@@ -1,5 +1,4 @@
 import { State, hookstate, useHookstate } from "@hookstate/core";
-
 export interface CriminalsDataType {
     name: string;
     age: number,
@@ -20,7 +19,14 @@ export interface PoliceAuthenticationType{
 
 
 const criminalsState = hookstate<Array<CriminalsDataType>>([]);
-const policesState = hookstate<Array<PoliceAuthenticationType>>([])
+const policesState = hookstate<Array<PoliceAuthenticationType>>([
+    {firstName: 'Benjamin', secondName: 'Davis', identificationNumber: '987654', registrationDate: '08/21/2012'},
+    {firstName: 'Olivia', secondName: 'Thompson', identificationNumber: '234567', registrationDate: '03/15/2018'},
+    {firstName: 'Liam', secondName: 'Anderson', identificationNumber: '876543', registrationDate: '11/30/2014'},
+    {firstName: 'Admin', secondName: '1', identificationNumber: '123456', registrationDate: '11/11/1111'},
+    {firstName: 'Ava', secondName: 'Martinez', identificationNumber: '345678', registrationDate: '09/07/2019'},
+    {firstName: 'Ethan', secondName: 'Wilson', identificationNumber: '789012', registrationDate: '06/25/2011'}
+])
 
 
 const wrapStateCriminals = (s: State<Array<CriminalsDataType>>) => ({
@@ -30,14 +36,13 @@ const wrapStateCriminals = (s: State<Array<CriminalsDataType>>) => ({
 
 const wrapStatePolices = (s: State<Array<PoliceAuthenticationType>>) => ({
     get: () => s.value,
-    set: (list: Array<PoliceAuthenticationType>) => s.set(list)
+    add: (list: PoliceAuthenticationType) => s.merge([list])
 })
 
 
 
 export const useGlobalStateCriminals = () => wrapStateCriminals(useHookstate(criminalsState))
-export const useGlobalStatePolices= () => wrapStateCriminals(useHookstate(criminalsState))
-
+export const useGlobalStatePolices = () => wrapStatePolices(useHookstate(policesState))
 
 
 
