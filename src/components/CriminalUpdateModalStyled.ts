fab7cc72011468cf6f0c5ce9bf752d6ce2928b4d
fernando <fernandoalanfillmann@gmail.com>
2023-08-16 10:30:58 -0300

    style: cria body estilizado do modal

---
+++ src/components/CriminalUpdateModalStyled.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const ModalBodyStyled = styled(Box)({
+    position: "absolute",
+    top: "0",
+    left: "0",
+    minHeight: "100vh",
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.3)"
+});
