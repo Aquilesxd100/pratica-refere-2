43d34bb14984e1759c70bd3eb899ee469d67d6fe
fernando <fernandoalanfillmann@gmail.com>
2023-08-16 10:31:22 -0300

    feat: cria componente modal update de criminal

---
+++ src/components/CriminalUpdateModal.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ModalBodyStyled } from './CriminalUpdateModalStyled';
+
+/* interface CriminalUpdateModalProps {
+  property: string;
+} */
+
+const CriminalUpdateModal: React.FC = () => {
+  return (
+    <ModalBodyStyled>
+
+    </ModalBodyStyled>
+  );
+};
+
+export default CriminalUpdateModal;
